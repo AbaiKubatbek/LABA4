b443e335582564f4feb3fe9fb62c304b3320ab4e
AbaiKubatbek <abai.kubatbekov.02@gmail.com>
2023-03-04 10:20:33 +0600

    first commit

---
+++ src/Models/EatAndDrinkDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EatAndDrink.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EatAndDrinkDbContext : DbContext
+    {
+        // TODO: Нужно обновить строку подключения
+        // Вместо <сервер> указываем имя сервера SQL Server Expres
+        // Вместо <пароль> указываем пароль созданный при установке SQL Server Expres
+        private readonly String _connectionString = "Data Source=DESKTOP-FEQVEMS; Initial Catalog=EatAndDrinkDB;" +
+          "Persist Security Info=True;TrustServerCertificate=True;User ID = SA; Password=0700415926a";
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_connectionString);
+    }
+}
